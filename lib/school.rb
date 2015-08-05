02f8a656cfdb4338ff2f6edcfa10537d9e1debf6
George Toothman <george.toothman@gmail.com>
2015-08-05 17:24:16 -0400

    add school roster fix

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # Write code here
+
+class School
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort  
+    @roster.each_value do |students_array|
+      students_array.sort!
+    end
+  end
+end
